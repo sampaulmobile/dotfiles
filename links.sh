495a8de49f139c247c8c4ad43da79583a35d59ed
Sam Paul <sam@docurated.com>
2017-02-06 22:11:54 -0500

    touched up setup.sh/links.sh and removed some unnecessary lines

--- links.sh
+++ links.sh
@@ -26,39 +26,43 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-echo "Linking in vundles"
-mkdir -p ~/.vim
-mv ~/.vim/vundles.vim $deldir
-ln -s $dir/vundles.vim ~/.vim/vundles.vim
-echo "Done"
-
-echo "Linking in custom theme (sampaul)"
-mv ~/.oh-my-zsh/custom/sampaul.zsh-theme $deldir
-ln -s $dir/sampaul.zsh-theme ~/.oh-my-zsh/custom/sampaul.zsh-theme
-echo "Done"
-
 echo "Linking SSH config"
 mkdir -p ~/.ssh
 mv ~/.ssh/config $deldir
 ln -s $dir/ssh_config ~/.ssh/config
 echo "Done"
 
-echo "Linking AWS config"
-mkdir -p ~/.aws
-mv ~/.aws/config $deldir
-ln -s $NOT_PUBLIC/aws_config ~/.aws/config
+echo "Linking in vundles"
+mkdir -p ~/.vim
+mv ~/.vim/vundles.vim $deldir
+ln -s $dir/vundles.vim ~/.vim/vundles.vim
 echo "Done"
 
-echo "Linking AWS (shared) credentials"
-mkdir -p ~/.aws
-mv ~/.aws/credentials $deldir
-ln -s $NOT_PUBLIC/aws_credentials ~/.aws/credentials
-echo "Done"
+if [ -d ~/.oh-my-zsh ]; then
+    echo "Linking in custom theme (sampaul)"
+    mv ~/.oh-my-zsh/custom/sampaul.zsh-theme $deldir
+    ln -s $dir/sampaul.zsh-theme ~/.oh-my-zsh/custom/sampaul.zsh-theme
+    echo "Done"
+fi
 
-echo "Linking pgpass"
-mv ~/.pgpass $deldir
-ln -s $NOT_PUBLIC/pgpass ~/.pgpass
-echo "Done"
+if [ -d $NOT_PUBLIC ]; then
+    echo "Linking AWS config"
+    mkdir -p ~/.aws
+    mv ~/.aws/config $deldir
+    ln -s $NOT_PUBLIC/aws_config ~/.aws/config
+    echo "Done"
+
+    echo "Linking AWS (shared) credentials"
+    mkdir -p ~/.aws
+    mv ~/.aws/credentials $deldir
+    ln -s $NOT_PUBLIC/aws_credentials ~/.aws/credentials
+    echo "Done"
+
+    echo "Linking pgpass"
+    mv ~/.pgpass $deldir
+    ln -s $NOT_PUBLIC/pgpass ~/.pgpass
+    echo "Done"
+fi
 
 echo "ALL FINISHED LINKING RCs"
 echo "DELETING old dotfiles"
--- setup.sh
+++ setup.sh
@@ -3,18 +3,10 @@
 DOTFILES=~/dotfiles
 
 echo "Installing homebrew..."
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Tapping caskroom..."
 brew tap caskroom/cask
-
-echo "Installing ZSH..."
-brew install zsh
-echo "Setting ZSH to default shell..."
-sudo chsh -s /bin/zsh
-
-echo "Installing Oh-My-ZSH..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)”
 
 echo "Installing VIm + MacVIm (Make sure XCode is installed/run)"
 brew install vim
@@ -26,45 +18,54 @@ brew install the_silver_searcher
 echo "Installing Chrome..."
 brew cask install google-chrome
 
-# echo "Installing HipChat..."
-# brew cask install hipchat
-
 echo "Installing iTerm2..."
 brew cask install iterm2
 
 echo "Installing Dropbox..."
 brew cask install dropbox
 
-# echo "Installing VLC..."
-# brew cask install vlc
-
 echo "Installing iStat-Menus..."
 brew cask install istat-menus
-
-echo "Installing uTorrent..."
-brew cask install utorrent
 
 echo "Installing Dash..."
 brew cask install dash
 
-# brew cask install skype
+echo "Installing Slack..."
+brew cask install slack
 
 echo "Installing 1password..."
 brew cask install 1password
 
+echo "Installing RVM... (+Ruby 2.2.5)"
+\curl -sSL https://get.rvm.io | bash
+source /Users/sampaul/.rvm/scripts/rvm
+rvm install ruby-2.2.5
+
+# echo "Installing uTorrent..."
+# brew cask install utorrent
+# echo "Installing VLC..."
+# brew cask install vlc
+# brew cask install skype
+# echo "Installing HipChat..."
+# brew cask install hipchat
+
+# echo "Setting up GIT"
+# ssh-keygen -t rsa -b 4096 -C "sampaulmobile@gmail.com"
+# pbcopy < ~/.ssh/id_rsa.pub
+# echo "key copied to clipboard, paste into https://github.com/settings/ssh"
+
 echo "Setting up dotfiles..."
 git clone git@github.com:sampaulmobile/dotfiles.git $DOTFILES
 $DOTFILES/links.sh
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd ~ && vim +PluginInstall +qall
 
-echo "Installing RVM... (+Ruby 1.9.3)"
-\curl -sSL https://get.rvm.io | bash
-source /Users/sampaul/.rvm/scripts/rvm
-rvm install ruby-1.9.3
+echo "Installing ZSH..."
+brew install zsh
+echo "Setting ZSH to default shell..."
+sudo chsh -s /bin/zsh
 
-echo "Setting up GIT"
-ssh-keygen -t rsa -b 4096 -C "sampaulmobile@gmail.com"
-pbcopy < ~/.ssh/id_rsa.pub
-echo "key copied to clipboard, paste into https://github.com/settings/ssh"
+echo "Installing Oh-My-ZSH..."
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
