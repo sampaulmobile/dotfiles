24fb541d165440ccf3f6f0d4dc5065c30eeb79b3
Sam Paul <sam@docurated.com>
2017-02-07 17:32:56 -0500

    added checks to setup.sh/links.sh for already installed packages

--- links.sh
+++ links.sh
@@ -14,62 +14,53 @@ NOT_PUBLIC=$HOME/dropbox/!public
 ##########
 
 # create DELETE_dotfiles in homedir
-echo -n "Creating $deldir..."
 mkdir -p $deldir
-echo "done"
 
 # Remove existing dotfiles and create symlinks for $files
 for file in $files; do
-    echo "Backing up ~/.$file"
+    echo "Linking $file..."
     mv ~/.$file $deldir
-    echo "Symlinking to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
-echo "Linking SSH config"
+echo "Linking SSH config..."
 mkdir -p ~/.ssh
 mv ~/.ssh/config $deldir
 ln -s $dir/ssh_config ~/.ssh/config
-echo "Done"
 
-echo "Linking in vundles"
+echo "Linking vundles..."
 mkdir -p ~/.vim
 mv ~/.vim/vundles.vim $deldir
 ln -s $dir/vundles.vim ~/.vim/vundles.vim
-echo "Done"
 
 if [ -d ~/.oh-my-zsh ]; then
-    echo "Linking in custom theme (sampaul)"
+    echo "Linking in custom theme (sampaul)..."
     mv ~/.oh-my-zsh/custom/sampaul.zsh-theme $deldir
     ln -s $dir/sampaul.zsh-theme ~/.oh-my-zsh/custom/sampaul.zsh-theme
-    echo "Done"
 fi
 
 if [ -d $NOT_PUBLIC/links ]; then
-    echo "Linking AWS config"
+    echo "Linking AWS config..."
     mkdir -p ~/.aws
     mv ~/.aws/config $deldir
     ln -s $NOT_PUBLIC/links/aws_config ~/.aws/config
-    echo "Done"
 
-    echo "Linking AWS (shared) credentials"
+    echo "Linking AWS (shared) credentials..."
     mkdir -p ~/.aws
     mv ~/.aws/credentials $deldir
     ln -s $NOT_PUBLIC/links/aws_credentials ~/.aws/credentials
-    echo "Done"
 
-    echo "Linking ssh known_hosts"
+    echo "Linking ssh known_hosts..."
     mv ~/.ssh/known_hosts $deldir
     ln -s $NOT_PUBLIC/links/ssh_known_hosts ~/.ssh/known_hosts
-    echo "Done"
 
-    echo "Linking pgpass"
+    echo "Linking pgpass..."
     mv ~/.pgpass $deldir
     ln -s $NOT_PUBLIC/links/pgpass ~/.pgpass
-    echo "Done"
 fi
 
 echo "ALL FINISHED LINKING RCs"
 echo "DELETING old dotfiles"
 rm -rf $deldir
+echo "Done."
 
--- setup.sh
+++ setup.sh
@@ -1,54 +1,108 @@
 #!/bin/bash
 
-DOTFILES=~/dotfiles
+DOTFILES=$HOME/dotfiles
 
 echo "Linking up (available) dotfiles..."
 $DOTFILES/links.sh
 
-echo "Installing homebrew..."
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Brew / Cask installing stuff..."
+
+if [ ! -f /usr/local/bin/brew ]; then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if [ ! -f /usr/local/bin/mvim ]; then
+    echo "Installing VIm + MacVIm (Make sure XCode is installed/run)"
+    brew install vim
+    brew install macvim
+fi
+
+if [ ! -d '/usr/local/Cellar/the_silver_searcher' ]; then
+    echo "Installing the Silver Searcher"
+    brew install the_silver_searcher
+fi
 
 echo "Tapping caskroom..."
 brew tap caskroom/cask
 
-echo "Installing VIm + MacVIm (Make sure XCode is installed/run)"
-brew install vim
-brew install macvim
+if [ ! -d "/Applications/Google Chrome.app" ]; then
+    echo "Installing Chrome..."
+    brew cask install google-chrome
+fi
 
-echo "Installing the Silver Searcher"
-brew install the_silver_searcher
+if [ ! -d "/Applications/iTerm.app" ]; then
+    echo "Installing iTerm2..."
+    brew cask install iterm2
+fi
 
-echo "Installing Chrome..."
-brew cask install google-chrome
+if [ ! -d "/Applications/Dropbox.app" ]; then
+    echo "Installing Dropbox..."
+    brew cask install dropbox
+fi
 
-echo "Installing iTerm2..."
-brew cask install iterm2
+if [ ! -d "/Applications/Slack.app" ]; then
+    echo "Installing Slack..."
+    brew cask install slack
+fi
 
-echo "Installing Dropbox..."
-brew cask install dropbox
+if [ ! -d "/Applications/1Password 6.app" ]; then
+    echo "Installing 1password..."
+    brew cask install 1password
+fi
 
-echo "Installing iStat-Menus..."
-brew cask install istat-menus
+if [ ! -d "/Applications/iStat Menus.app" ]; then
+    echo "Installing iStat-Menus..."
+    brew cask install istat-menus
+fi
 
-echo "Installing Dash..."
-brew cask install dash
+if [ ! -d "/Applications/Dash.app" ]; then
+    echo "Installing Dash..."
+    brew cask install dash
+fi
 
-echo "Installing Slack..."
-brew cask install slack
+if [ ! -d "/usr/local/Caskroom/witch" ]; then
+    echo "Installing Witch..."
+    brew cask install witch
+fi
 
-echo "Installing 1password..."
-brew cask install 1password
+if [ ! -d "/Applications/VLC.app" ]; then
+    echo "Installing VLC..."
+    brew cask install vlc
+fi
 
-echo "Installing Witch..."
-brew cask install witch
+if [ ! -d $HOME/.rvm ]; then
+    echo "Installing RVM... (+Ruby 2.2.5)"
+    \curl -sSL https://get.rvm.io | bash
+    source $HOME/.rvm/scripts/rvm
+    rvm install ruby-2.2.5
+fi
 
-echo "Installing VLC..."
-brew cask install vlc
+if [ ! -d $HOME/.pyenv ]; then
+    echo "Installing pyenv... (+Python 3.5.2)"
+    brew install pyenv
+    pyenv install 3.5.2
+    pyenv global 3.5.2
+fi
 
-echo "Installing RVM... (+Ruby 2.2.5)"
-\curl -sSL https://get.rvm.io | bash
-source /Users/sampaul/.rvm/scripts/rvm
-rvm install ruby-2.2.5
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+
+echo "Updating vundle plugins..."
+cd $HOME && vim +PluginUpdate +qall
+
+if [ ! -f /bin/zsh ]; then
+    echo "Installing ZSH..."
+    brew install zsh
+    echo "Setting ZSH to default shell..."
+    sudo chsh -s /bin/zsh
+fi
+
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    echo "Installing Oh-My-ZSH..."
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # echo "Installing uTorrent..."
 # brew cask install utorrent
@@ -61,15 +115,4 @@ rvm install ruby-2.2.5
 # ssh-keygen -t rsa -b 4096 -C "sampaulmobile@gmail.com"
 # pbcopy < ~/.ssh/id_rsa.pub
 # echo "key copied to clipboard, paste into https://github.com/settings/ssh"
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ~ && vim +PluginInstall +qall
-
-echo "Installing ZSH..."
-brew install zsh
-echo "Setting ZSH to default shell..."
-sudo chsh -s /bin/zsh
-
-echo "Installing Oh-My-ZSH..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
