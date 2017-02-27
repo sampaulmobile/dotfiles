397066c7b5b1d360a9e2eefafb3e0795dc43b51c
Sam Paul <sam@docurated.com>
2017-02-27 10:18:18 -0500

    added macos, and cleaned up setup.sh a little bit

---
+++ macos.sh
@@ -0,0 +1,131 @@
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Always show scrollbars
+# Possible values: `WhenScrolling`, `Automatic` and `Always`
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+
+# Stop iTunes from responding to the keyboard media keys
+#launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "jpg"
+
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+# defaults write com.apple.finder NewWindowTarget -string "PfDe"
+# defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Disable the warning when changing a file extension
+# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Top left screen corner → Screen Saver
+defaults write com.apple.dock wvous-tl-corner -int 5
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right screen corner → Desktop
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 3
+defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right screen corner → Mission Control
+defaults write com.apple.dock wvous-br-corner -int 2
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don’t use
+# the Dock to launch apps.
+defaults write com.apple.dock persistent-apps -array
+
+# Show only open applications in the Dock
+defaults write com.apple.dock static-only -bool true
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+
+for app in 	"Dock" "Finder"; do
+	killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
--- setup.sh
+++ setup.sh
@@ -1,84 +1,41 @@
 #!/bin/bash
 
-DOTFILES=$HOME/dotfiles
-USERNAME=sampaul
 OPT_DIR=/usr/local/opt
 
-# xcode-select --install
+# DOTFILES / LINKING
 
-echo "Making dock static-only..."
-# defaults write com.apple.dock static-only -bool TRUE; killall Dock
-echo "Enabling key repeat..."
-# defaults write -g ApplePressAndHoldEnabled -bool false
-# defaults write -g InitialKeyRepeat -int 15
-# defaults write -g KeyRepeat -int 2
+$HOME/dotfiles/links.sh
 
-echo "Linking up (available) dotfiles..."
-$DOTFILES/links.sh
 
-echo "Brew / Cask installing stuff..."
+# INSTALLS
+
+xcode-select --install
 
 if [ ! -f /usr/local/bin/brew ]; then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-if [ ! -f /usr/local/bin/mvim ]; then
-    echo "Installing VIm + MacVIm (Make sure XCode is installed/run)"
-    brew install vim
-    brew install macvim
-    brew install gpg
-fi
+brew install zsh
+brew install vim
+brew install macvim
+brew install gpg
+brew install the_silver_searcher
 
-if [ ! -f /usr/local/bin/consul ]; then
-    brew install consul
-fi
+brew install consul
+brew install jq
+brew install awscli
 
-if [ ! -f /usr/local/bin/jq ]; then
-    brew install jq
-fi
+brew install scala
+brew install apache-spark
+brew install cmake
 
-if [ ! -f /usr/local/bin/aws ]; then
-    brew install awscli
-fi
+brew install pyenv
 
-if [ ! -f /usr/local/bin/psql ]; then
-    echo "Installing postgres..."
-    brew install postgresql
-    brew services start postgresql
-    echo "creating postgres user..."
-    createuser $1 -P -s
-fi
+brew install postgresql
+brew install redis
+brew install mongo
 
-if [ ! -f /usr/local/bin/redis-server ]; then
-    echo "Installing redis..."
-    brew install redis
-    brew services start redis
-fi
-
-if [ ! -f /usr/local/bin/mongo ]; then
-    echo "Installing mongo..."
-    brew install mongo
-    brew services start mongodb
-    # use docurated
-    # db.createUser({user: "sampaul", pwd: "password", roles: ["readWrite"]})
-    # mongo admin -u admin -p admin --eval "db.getSiblingDB('docurated').addUser('sampaul', 'password')"
-fi
-
-if [ ! -f /usr/local/bin/scala ]; then
-    echo "Installing scala..."
-    brew install scala
-fi
-
-if [ ! -f /usr/local/bin/pyspark ]; then
-    echo "Installing spark..."
-    brew install apache-spark
-fi
-
-if [ ! -d '/usr/local/Cellar/the_silver_searcher' ]; then
-    echo "Installing the Silver Searcher"
-    brew install the_silver_searcher
-fi
 
 echo "Tapping caskroom..."
 brew tap caskroom/cask
@@ -139,25 +96,25 @@ if [ ! -d "/Applications/Tunnelblick.app" ]; then
     brew cask install tunnelblick
 fi
 
+
+# CONFIGURATION
+
+if [ "$SHELL" != "/usr/local/bin/zsh" ]; then
+    echo "Setting ZSH to default shell..."
+    sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+fi
+
 if [ ! -d $HOME/.pyenv ]; then
-    echo "Installing pyenv... (+Python 3.5.2)"
-    brew install pyenv
-    # pyenv install 3.5.2
+    echo "Installing Python 3.5.2"
     PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.5.2
     pyenv global 3.5.2
 fi
 
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+    echo "Installing Vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
-
-echo "Updating vundle plugins..."
 cd $HOME && vim +PluginClean! +PluginUpdate +qall
-
-if [ ! -f /usr/local/bin/cmake ]; then
-    echo "Installing cmake..."
-    brew install cmake
-fi
 
 if [ ! -f $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_core.so ]; then
     echo "Building/installing YouCompleteMe..."
@@ -166,11 +123,23 @@ if [ ! -f $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_core.so ]; then
     cd
 fi
 
-if [ ! -f /bin/zsh ]; then
-    echo "Installing ZSH..."
-    brew install zsh
-    echo "Setting ZSH to default shell..."
-    sudo chsh -s /bin/zsh
+if [ ! -f /usr/local/bin/psql ]; then
+    echo "Setting up postgres..."
+    brew services start postgresql
+    createuser $USER -P -s
+fi
+
+if [ ! -f /usr/local/bin/redis-server ]; then
+    echo "Setting up redis..."
+    brew services start redis
+fi
+
+if [ ! -f /usr/local/bin/mongo ]; then
+    echo "Setting up mongo..."
+    brew services start mongodb
+    # use docurated
+    # db.createUser({user: "sampaul", pwd: "password", roles: ["readWrite"]})
+    # mongo admin -u admin -p admin --eval "db.getSiblingDB('docurated').addUser('sampaul', 'password')"
 fi
 
 if [ ! -d $HOME/.oh-my-zsh ]; then
@@ -191,7 +160,6 @@ if [ ! -d $OPT_DIR/zsh-git-prompt ]; then
     stack build && stack install
 fi
 
-
 # restart to install rvm
 
 if [ ! -d $HOME/.rvm ]; then
@@ -200,18 +168,4 @@ if [ ! -d $HOME/.rvm ]; then
     source $HOME/.rvm/scripts/rvm
     rvm install ruby-2.2.5
 fi
-
-# install java
-
-# echo "Installing uTorrent..."
-# brew cask install utorrent
-# echo "Installing Skype..."
-# brew cask install skype
-# echo "Installing HipChat..."
-# brew cask install hipchat
-
-# echo "Setting up GIT"
-# ssh-keygen -t rsa -b 4096 -C "sampaulmobile@gmail.com"
-# pbcopy < ~/.ssh/id_rsa.pub
-# echo "key copied to clipboard, paste into https://github.com/settings/ssh"
 
