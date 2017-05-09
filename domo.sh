afb915a168dac390465c64427263e975dd2eb4f9
Sam Paul <sam@docurated.com>
2017-05-08 21:47:03 -0400

    adding domo.sh

---
+++ domo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# add SSH keys somewhere
+# ssh-add them too
+
+# INSTALLS
+
+xcode-select --install
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install zsh
+brew install vim
+
+echo "Tapping caskroom..."
+brew tap caskroom/cask
+
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install witch
+brew cask install vlc
+
+brew cask install sabnzbd
+brew cask install couchpotato
+brew cask install sonarr
+
+# CONFIGURATION
+
+sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
