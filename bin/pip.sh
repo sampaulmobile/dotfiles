9da78bd8abc2aea04aa532f99f579fccee210941
Sam Paul <sam@docurated.com>
2017-11-07 16:47:44 -0500

    small fixes for pip.sh, added strip whitespace vim mapping

--- bin/pip.sh
+++ bin/pip.sh
@@ -1,13 +1,16 @@
 
-if [ ! -f /usr/local/bin/brew ]; then
-	curl -o $HOME/downloads/get-pip.py https://bootstrap.pypa.io/get-pip.py
-	python $HOME/downloads/get-pip.py
-	rm $HOME/downloads/get-pip.py
+if [ ! -f /usr/local/bin/pip ]; then
+    curl -o $HOME/downloads/get-pip.py https://bootstrap.pypa.io/get-pip.py
+    sudo python $HOME/downloads/get-pip.py
+    rm $HOME/downloads/get-pip.py
 fi
 
 if [ ! -d $HOME/.pyenv ]; then
     echo "Installing Python 3.5.2"
-    PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.5.2
+    CFLAGS="-I$(brew --prefix openssl)/include" \
+    LDFLAGS="-L$(brew --prefix openssl)/lib" \
+    PYTHON_CONFIGURE_OPTS="--enable-framework" \ # required for haskell YCM
+    pyenv install 3.5.2
     pyenv global 3.5.2
 fi
 
