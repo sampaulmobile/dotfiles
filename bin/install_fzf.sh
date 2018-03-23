433c942dc231d45cf5455bcfac49052afddb1e6c
Sam Paul <sam@docurated.com>
2018-03-22 23:31:45 -0400

    no need to update rc when installing fzf

--- bin/install_fzf.sh
+++ bin/install_fzf.sh
@@ -1,5 +1,5 @@
 if [ ! -d $HOME/.fzf ]; then
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install --completion --key-bindings --update-rc
+	~/.fzf/install --completion --key-bindings --no-update-rc
 	exec "$SHELL"
 fi
