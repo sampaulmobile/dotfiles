6c8a1a734a3153ea98ed96227f377ab9c59b9a25
Sam Paul <sam@docurated.com>
2018-05-06 18:47:04 -0400

    improved root_setup_ubuntu to create user .ssh folder and download pub key/chmod it

--- root_setup_ubuntu.sh
+++ root_setup_ubuntu.sh
@@ -1,3 +1,8 @@
+NEW_USER=sampaul
+NEW_PASS=password
+NEW_HOST=sbp_dev
+PUB_KEY_URL=https://www.dropbox.com/s/rwvh7mfqwm4zx0f/id_rsa_linode.pub?dl=0
+
 # install stuff
 apt-get update
 apt-get upgrade -y
@@ -6,15 +11,20 @@ apt-get install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
     libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
 
 # create user and assign password
-NEW_USER=sampaul
-NEW_HOSTNAME=sbp_dev
-NEW_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-
 adduser $NEW_USER --gecos "" --disabled-password
-echo "$NEW_USER:password" | chpasswd
+echo "$NEW_USER:$NEW_PASS" | chpasswd
 adduser $NEW_USER sudo
 
-hostnamectl set-hostname $NEW_HOSTNAME
-echo "$NEW_IP $NEW_HOSTNAME.com $NEW_HOSTNAME" >> /etc/hosts
+mkdir /home/$NEW_USER/.ssh
+cd /home/$NEW_USER/.ssh
+curl -O -J -L $PUB_KEY_URL
+cp id_rsa_linode.pub authorized_keys
+chmod 600 /home/$NEW_USER/.ssh/*
+chmod 700 /home/$NEW_USER/.ssh
+chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh
+
+hostnamectl set-hostname $NEW_HOST
+EXT_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+echo "$EXT_IP $NEW_HOST.com $NEW_HOST" >> /etc/hosts
 
 # update /etc/ssh/sshd_config to reject root login/etc.
