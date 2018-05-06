80f5962b7d016110f55181baef398f4e898f6f0e
Sam Paul <sam@docurated.com>
2018-05-06 19:46:01 -0400

    switching back to master branch for oh_my_zsh

--- bin/install_oh_my_zsh.sh
+++ bin/install_oh_my_zsh.sh
@@ -2,5 +2,4 @@ if [ $# -eq 0 ] && [ -d $HOME/.oh-my-zsh ]; then
     exit 1
 fi
 
-# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/mriedmann/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
