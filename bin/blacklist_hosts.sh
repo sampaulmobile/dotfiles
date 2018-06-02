0122410b5aa1f1554f3b331bb3d29160d5e629a2
Sam Paul <sam@docurated.com>
2018-06-02 12:26:13 -0400

    updated blacklist_hosts.sh to just curl the latest fakenews hosts file

--- bin/blacklist_hosts.sh
+++ bin/blacklist_hosts.sh
@@ -3,9 +3,13 @@ if [ $# -eq 0 ] && grep -q "StevenBlack/hosts" /etc/hosts; then
 fi
 
 sudo -v
-tmp_dir=/tmp/hosts
 
-git clone https://github.com/StevenBlack/hosts.git $tmp_dir
-python $tmp_dir/updateHostsFile.py \
-	--backup --auto --replace --extensions fakenews --flush-dns-cache
-rm -rf $tmp_dir
+# tmp_dir=/tmp/hosts
+# git clone https://github.com/StevenBlack/hosts.git $tmp_dir
+# pip install --user -r $tmp_dir/requirements_python2.txt
+# python $tmp_dir/updateHostsFile.py \
+# 	--backup --auto --replace --extensions fakenews --flush-dns-cache
+# rm -rf $tmp_dir
+
+curl -o /tmp/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
+mv /tmp/hosts /etc/hosts
