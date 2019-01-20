cfbbc91385bb2547f387a2a92a2d15cbc0eabe2b
sampaulmobile <sampaulmobile@gmail.com>
2019-01-20 12:58:21 -0500

    reverting install_pyenv.sh to use exec "$SHELL"

--- bin/install_pyenv.sh
+++ bin/install_pyenv.sh
@@ -7,3 +7,4 @@ RCFILE=$HOME/.zshenv
 git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $RCFILE
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $RCFILE
+exec "$SHELL"
