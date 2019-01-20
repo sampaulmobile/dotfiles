c51db04f81a67e2430657ac364fd6ecc64aa1c31
sampaulmobile <sampaulmobile@gmail.com>
2019-01-20 14:01:24 -0500

    updated run_docker.sh script to take dev folder as param

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+DEV_DIR=${1:-$HOME/dev}
+
+if [ ! -d "$DEV_DIR" ]; then
+    echo "dev directory does not exist, exiting."
+    exit 1
+fi
+
 docker build -t dev .
-docker run -it dev
+docker run --rm -it \
+    --name dev \
+    -v $DEV_DIR:/root/dev \
+    dev:latest
