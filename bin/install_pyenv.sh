4063353d97a895af3a1871901542ef2271f8a446
sampaulmobile <sampaulmobile@gmail.com>
2019-01-20 02:40:55 -0500

    removing "exec" call from install_pyenv

--- bin/install_pyenv.sh
+++ bin/install_pyenv.sh
@@ -7,4 +7,3 @@ RCFILE=$HOME/.zshenv
 git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $RCFILE
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $RCFILE
-exec "$SHELL"
