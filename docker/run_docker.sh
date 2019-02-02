3412fc8082078169d6d27ed8e590c91b99628794
sampaulmobile <sampaulmobile@gmail.com>
2019-02-02 14:28:17 -0500

    cleaned up run_docker.sh a little bit

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -7,20 +7,16 @@ if [ ! -d "$DEV_DIR" ]; then
     exit 1
 fi
 
-# docker build -t dev .
-# docker run --rm -it \
-#     --name dev \
-#     --hostname dev-docker \
-#     --volume "$DEV_DIR":/root/dev \
-#     --volume /var/run/docker.sock:/var/run/docker.sock \
-#     --workdir /root/dev \
-#     dev:latest
+# DOCKERFILE=Dockerfile
+# NAME=dev
+DOCKERFILE=alpine.Dockerfile
+NAME=dev-alpine
 
-docker build -f alpine.Dockerfile -t dev-alpine .
+docker build -f $DOCKERFILE -t $NAME .
 docker run --rm -it \
-    --name dev-alpine \
+    --name $NAME \
     --hostname dev-docker \
     --volume "$DEV_DIR":/root/dev \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --workdir /root/dev \
-    dev-alpine:latest
+    $NAME:latest
