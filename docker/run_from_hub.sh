ab395866e3d8fc84961727f2d8746ce5c88e565e
sampaulmobile <sampaulmobile@gmail.com>
2019-02-02 15:12:03 -0500

    renamed the docker run files

---
+++ docker/run_from_hub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DEV_DIR=${1:-$HOME/dev}
+
+if [ ! -d "$DEV_DIR" ]; then
+    echo "dev directory does not exist, exiting."
+    exit 1
+fi
+
+# NAME=sampaul/dev
+NAME=sampaul/dev:alpine
+
+docker run --rm -it \
+    --name $NAME \
+    --hostname dev-docker \
+    --volume "$DEV_DIR":/root/dev \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --workdir /root/dev \
+    $NAME
