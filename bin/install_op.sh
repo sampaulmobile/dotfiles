1d17032ad2097076f971b5682424ddcdb633f03f
sampaulmobile <sampaulmobile@gmail.com>
2019-03-24 22:58:01 -0400

    updated install_op script

--- bin/install_op.sh
+++ bin/install_op.sh
@@ -2,6 +2,10 @@ if [ $# -eq 0 ] && [ -f /usr/local/bin/op ]; then
 	exit 1
 fi
 
-curl -o /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_linux_amd64_v0.5.5.zip
+if [ "$(uname -s)" = "Darwin" ]; then
+	curl -o /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_darwin_amd64_v0.5.5.zip
+else
+	curl -o /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_linux_amd64_v0.5.5.zip
+fi
 unzip /tmp/op.zip -d /tmp
 mv /tmp/op /usr/local/bin
