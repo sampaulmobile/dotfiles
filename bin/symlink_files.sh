de32892210a93a3cbd3d4ea7080b66bda6911f6f
SP <sampaulmobile@gmail.com>
2026-02-07 17:07:16 -0500

    few fixes

--- bin/symlink_files.sh
+++ bin/symlink_files.sh
@@ -41,33 +41,4 @@ link $dir/starship.toml ~/.config/starship.toml
 # ===== neovim (LazyVim config) =====
 link $dir/nvim ~/.config/nvim
 
-# ===== Mac-only dotfiles =====
-if [[ "$OS" == "Darwin" ]]; then
-    link $dir/ideavimrc ~/.ideavimrc
-
-    # Dropbox-linked sensitive files (Mac only)
-    NOT_PUBLIC=$HOME/Dropbox/!Public
-    if [ -d $NOT_PUBLIC/links ]; then
-        mkdir -p ~/.aws
-        link $NOT_PUBLIC/links/aws_config ~/.aws/config
-        link $NOT_PUBLIC/links/aws_credentials ~/.aws/credentials
-
-        mkdir -p ~/.ssh
-        link $NOT_PUBLIC/links/ssh_config ~/.ssh/config
-        link $NOT_PUBLIC/links/ssh_known_hosts ~/.ssh/known_hosts
-
-        link $NOT_PUBLIC/links/pgpass ~/.pgpass
-
-        if [ -d $HOME/.kube ]; then
-            link $NOT_PUBLIC/keys/sam.paul.vpn.crt ~/.kube/client.crt
-            link $NOT_PUBLIC/keys/sam.paul.vpn.key ~/.kube/client.key
-            link $NOT_PUBLIC/keys/docurated.intermediate.crt ~/.kube/ca.crt
-        fi
-
-        if [ -d "$HOME/Library/Application Support/Tunnelblick" ]; then
-            link $NOT_PUBLIC/keys/Configurations "$HOME/Library/Application Support/Tunnelblick/Configurations"
-        fi
-    fi
-fi
-
 echo "Symlinks created successfully!"
